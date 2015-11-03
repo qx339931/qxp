dbde42dbd4a8992992f8e47e22dec05366a6e46f
mldn <262990869@qq.com>
2015-11-03 11:52:28 +0800

    Change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello GIT !!!");
 		System.out.println("是个好孩子");
-		System.out.println("是个坏孩子");
 		System.out.println("世界你好 !!!");
+		System.out.println("是个坏孩子");
+		System.out.println("是个13坏孩子");
+		System.out.println("是个123坏孩子");
+		System.out.println("是个21312坏孩子");
 	}
 }
---
+++ Sal.java
@@ -0,0 +1,3 @@
+public class Sal{
+	private Double price ;
+}
