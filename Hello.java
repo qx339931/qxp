310dd7b3bfac0a9f130e06e7b7b48b60724f9dcf
mldn <262990869@qq.com>
2015-11-03 11:56:10 +0800

    Change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -5,8 +5,8 @@ public class Hello{
 		System.out.println("世界你好 !!!");
 		System.out.println("是个坏孩子");
 		System.out.println("是个13坏孩子");
-		System.out.println("是个123坏孩子");
-		System.out.println("是个21312坏孩子");
+		System.out.println("是个1as23坏孩子");
+		System.out.println("是asdas个21312坏孩子");
 		System.out.println("是ad2坏孩子");
 	}
 }
